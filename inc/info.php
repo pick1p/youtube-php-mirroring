a6f7d9fd38a5821e1010953e92938c696c346d7a
pick1p <shubiaochuang@outlook.com>
2016-12-06 12:18:31 +0800

    Update info.php

--- inc/info.php
+++ inc/info.php
@@ -3,7 +3,7 @@
 $ym='http://'.$_SERVER['SERVER_NAME'].'/';//你的站点域名。不可为空，否则解析器无法工作
 $parser='parser';//解析器目录,默认parser，如果本程序不是安装在根目录请加上上级路径，如abc/parser
 $home_v='UCcLK3j-XWdGBnt5bR9NJHaQ';//首页推荐视频用户身份标示
-$youtube_api='AIzaSyAntBZOF6TTjM-1U_dQ6bQqZEhsTYfHunA';//youtube视频API
+$youtube_api='AIzaSyAf1QLRSiZ2de32mO9QK9vdzoJjuKmXypo';//youtube视频API
 $gurl_api='AIzaSyAntBZOF6TTjM-1U_dQ6bQqZEhsTYfHunA';//谷歌缩址api，用于防止dns污染
 $title='Youtube直通车';//您的网站名字
 
